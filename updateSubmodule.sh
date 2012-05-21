8e0d275f0e0be8e3aed046c3bef7c911f206426a
Jia Deng <flyucarl@gmail.com>
2012-05-21 14:14:39 +0800

    PUlled down the update of submodule

--- updateSubmodule.sh
+++ updateSubmodule.sh
@@ -4,3 +4,6 @@ git submodule update
 #git submodule foreach git pull origin master
 #git submodule foreach git submodule init
 git submodule foreach git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull
+
